042444ae50d174c1214743a8f551b5acbcfb4dc2
Jan Henckens <jan@henckens.be>
2026-03-26 20:29:35 +0100

    Fixed install migration

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -28,12 +28,29 @@ class Install extends Migration
         $this->addForeignKey(null, '{{%standardsite_records}}', ['entryId'], '{{%elements}}', ['id'], 'CASCADE');
         $this->addForeignKey(null, '{{%standardsite_records}}', ['siteId'], '{{%sites}}', ['id'], 'CASCADE');
 
+        if (!$this->db->tableExists('{{%standardsite_connections}}')) {
+            $this->createTable('{{%standardsite_connections}}', [
+                'id' => $this->primaryKey(),
+                'handle' => $this->string(255)->notNull(),
+                'did' => $this->string(255)->notNull(),
+                'pdsUrl' => $this->string(512)->notNull(),
+                'accessToken' => $this->text()->null(),
+                'refreshToken' => $this->text()->null(),
+                'dpopKey' => $this->text()->null(),
+                'tokenExpiresAt' => $this->integer()->null(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+            ]);
+        }
+
         return true;
     }
 
     public function safeDown(): bool
     {
         $this->dropTableIfExists('{{%standardsite_records}}');
+        $this->dropTableIfExists('{{%standardsite_connections}}');
         return true;
     }
 }
